175c875c67e24e43160dde6ac3c07a8a8ddb9839
Vasiliy <mazurboss2016@gmail.com>
2022-05-30 00:31:06 +0300

    2 tusk finished

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const makeGallery = galleryItems
+  .map(
+    image =>
+      `<a href="${image.original}">
+                <img
+                src="${image.preview}"
+                alt="${image.description}"
+                />
+            </a>`,
+  )
+  .join('');
+
+gallery.innerHTML = makeGallery;
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+  close: false,
+  showCounter: false,
+});
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const NAME_LOCAL_STORAGE = 'videoplayer-current-time';
 
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(item) {
+  localStorage.setItem(NAME_LOCAL_STORAGE, item.seconds);
+}
+
+const getSeconds = localStorage.getItem(NAME_LOCAL_STORAGE);
+
+if (getSeconds) {
+  player.setCurrentTime(getSeconds);
+}
